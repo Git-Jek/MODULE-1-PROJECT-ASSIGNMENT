ab67a634975889468149f06a5faa9a476070b103
Git-Jek <119988051+Git-Jek@users.noreply.github.com>
2023-01-06 21:53:43 +0800

    Added search, sort functionality in main.js and automated tests in main.test.js

--- main.js
+++ main.js
@@ -16,11 +16,23 @@ export var newsList = [
   "Marie Colvin shined a light on war-torn corners of the world",
 ];
 
-export function search() {
+export function search(newsList) {
   //inputValue is the variable that contains the search string
-  const inputValue = document.getElementById("search-input").value;
+
+  //const inputValue = document.getElementById("search-input").value;
+
+  const inputValue = "What"// (FOR TESTING)
 
   //Write your code here for the search function
+  newsList.forEach((news) => {
+    if (news.toString().includes(inputValue)){
+      newsList = newsList.filter(checkString);
+
+      function checkString(string){
+        return string.includes(inputValue)
+    }
+    };
+  });
 
   return newsList;
 }
@@ -28,8 +40,11 @@ export function search() {
 export function sort(type) {
   if (type == "ascending") {
     //Write your code here for sorting (ascending)
+    return newsList.sort()
   } else {
     //Write your code here for sorting (descending)
+    (type == "descending")
+    return newsList.sort().reverse()
   }
 
   return newsList;
--- main.test.js
+++ main.test.js
@@ -2,5 +2,34 @@ import { sort, search, newsList } from "./main.js";
 
 //Example testing for search function
 test("Search function testing", () => {
-  expect(search(newsList)).toBe("I am a cool web developer");
+  expect(search(newsList)).toStrictEqual( [
+    'What films should an aspiring journalist watch?',
+    'What books should an aspiring journalist read?'
+  ]);
+});
+
+test ("Sort in ascending order function testing", () => {
+  expect(sort("ascending")).toStrictEqual([
+    "'Nightmare' TV show 'Euphoria — health threat or high art?",
+    'Buried underpants and tea bags help scientists evaluate soil',
+    'Decoder: Armenia in a bind as Ukraine war resets global order',
+    'Decoder: Mining asteroids for minerals can help spare Earth',
+    'Marie Colvin shined a light on war-torn corners of the world',
+    'Media glare can enrich tennis pros yet imperil mental health',
+    'What books should an aspiring journalist read?',
+    'What films should an aspiring journalist watch?'
+  ]);
+});
+
+test ("Sort in descending order function testing", () => {
+  expect(sort("descending")).toStrictEqual([
+    'What films should an aspiring journalist watch?',
+    'What books should an aspiring journalist read?',
+    'Media glare can enrich tennis pros yet imperil mental health',
+    'Marie Colvin shined a light on war-torn corners of the world',
+    'Decoder: Mining asteroids for minerals can help spare Earth',
+    'Decoder: Armenia in a bind as Ukraine war resets global order',
+    'Buried underpants and tea bags help scientists evaluate soil',
+    "'Nightmare' TV show 'Euphoria — health threat or high art?"
+  ]);
 });
